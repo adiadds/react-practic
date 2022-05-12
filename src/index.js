3e98d73d2392db65a18b754be53e3db8d7fd6186
Adil Asad <aadilasadhatti@gmail.com>
2022-05-12 15:27:17 +0500

    props, props destructing, children props, object passing as props

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// first object
 const firstBook = {
   img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
   title: "I Love You to the Moon and Back",
   author: "Amelia Hepworth",
 };
-
+// second object
 const secondBook = {
   img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL604_SR604,400_.jpg",
   title: "If Animals Kissed Good Night",
@@ -17,27 +18,31 @@ const secondBook = {
 function BookList() {
   return (
     <section className="booklist">
+      {/* passsing objct 1 properties as props */}
       <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
+        {/* passsing children props */}
         <p>hi this is discription of the picture. and its children this is children props</p>
       </Book>
+      {/* passsing objct 2 properties as props */}
       <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props; //destructing
+  const { img, title, author } = props; //destructing method # 1
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      {/* receving children props. */}
       {props.children}
     </article>
   );
 };
 
-// //destructing
+//destructing method # 2
 // const Book = ({ img, title, author, children }) => {
 //   return (
 //     <article className="book">
@@ -49,6 +54,7 @@ const Book = (props) => {
 //   );
 // };
 
+// without destructing
 // const Book = (props) => {
 //   return (
 //     <article className="book">
