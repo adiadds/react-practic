c67344e75bc270baf6090b586b2080f12d810154
Adil Asad <aadilasadhatti@gmail.com>
2022-05-12 14:09:30 +0500

    destructing and object transfering

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL604_SR604,400_.jpg",
+  title: "If Animals Kissed Good Night",
+  author: "Ann Whitford Paul",
+};
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
+        <p>hi this is discription of the picture. and its children this is children props</p>
+      </Book>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const { img, title, author } = props; //destructing
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {props.children}
+    </article>
+  );
+};
+
+// //destructing
+// const Book = ({ img, title, author, children }) => {
+//   return (
+//     <article className="book">
+//       <img src={img} alt="" />
+//       <h1>{title}</h1>
+//       {children}
+//       <h4>{author}</h4>
+//     </article>
+//   );
+// };
+
+// const Book = (props) => {
+//   return (
+//     <article className="book">
+//       <img src={props.img} alt="" />
+//       <h1>{props.title}</h1>
+//       <h4>{props.author}</h4>
+//     </article>
+//   );
+// };
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
